3e50a85ee7bf8a268aa0e3027497fe3f7de43128
name <lgu@gmx.at>
2022-11-07 13:16:51 +0100

    until lesson 38

--- pages/products/[url].jsx
+++ pages/products/[url].jsx
@@ -5,11 +5,27 @@ import Image from "next/image";
 import mongodb from "../../utils/mongodb";
 import Product from "../../models/Product";
 import { ListGroup, Button, ListGroupItem } from "react-bootstrap";
+import { useState } from "react";
 
-export default function Productsite({product}) {
+export default function Productsite({ product }) {
     //const router = useRouter();
     //const { url } = router.query;
     //const product = jsondb.products.find(a => a.url === url);
+
+    const [price, setPrice] = useState(product.price);
+    const [extras, setExtras] = useState([]);
+    const [quantity, setQuantity] = useState(1);
+
+    const addExtra = (e, extra) => {
+        const checked = e.target.checked;
+        if (checked) {
+            setPrice(price + extra.price);
+            setExtras([...extras, extra])
+        } else {
+            setPrice(price - extra.price);
+            setExtras(extras.filter((allExtras) => allExtras._id !== extra._id))
+        }
+    }
 
     if (!product) {
         return (
@@ -17,7 +33,7 @@ export default function Productsite({product}) {
                 <h2>Product nicht vorhanden</h2>
                 <Link href="/">
                     <a className="text-dark">
-                    &#129044; zurück zur Übersicht
+                        &#129044; zurück zur Übersicht
                     </a>
                 </Link>
             </div>
@@ -42,7 +58,7 @@ export default function Productsite({product}) {
                     <h1>{product.designation}</h1>
                     <ListGroup variant="flush">
                         <ListGroupItem>
-                            <h2 className="text-danger">{product.price} €</h2>
+                            <h2 className="text-danger">{price.toFixed(2)} €</h2>
                         </ListGroupItem>
                         <ListGroupItem>
                             {product.description}
@@ -50,8 +66,12 @@ export default function Productsite({product}) {
                         <ListGroupItem>
                             {product.extras.length ? "Extras:" : <p></p>}
                             {product.extras.map((extra) => (
-                                <span key={extra.name}>
-                                    {extra.text}<input className="form-check-input me-2" type="checkbox" />
+                                <span key={extra._id}>
+                                    {extra.text}<input className="form-check-input me-2"
+                                        type="checkbox"
+                                        id={extra.text}
+                                        onChange={(e) => addExtra(e, extra)}
+                                    />
                                 </span>
                             ))}
                             {/* Extras:
@@ -59,7 +79,9 @@ export default function Productsite({product}) {
                             extra Pommes <input className="form-check-input me-2" type="checkbox" /> */}
                         </ListGroupItem>
                         <ListGroupItem>
-                            <input className="form-control w-50" type="number" placeholder="1" min="1"></input>
+                            <input className="form-control w-50" type="number" value={quantity} min="1" max="100"
+                            onChange={(e) => setQuantity(e.target.value)}
+                            ></input>
                         </ListGroupItem>
                         <ListGroupItem>
                             <div className="row shadow">
@@ -78,11 +100,10 @@ export default function Productsite({product}) {
 export async function getServerSideProps(context) {
     const url = context.params.url;
     await mongodb.dbConnect();
-    const product = await Product.findOne({url}).lean();
+    const product = await Product.findOne({ url }).lean();
     return {
-      props: {
-        product: JSON.parse(JSON.stringify(product))
-      }
+        props: {
+            product: JSON.parse(JSON.stringify(product))
+        }
     }
-  }
- 
+}
